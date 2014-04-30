dc78d9f6275068f8247fe4035a7c2391f645f7e7
Samuel Gervais <vals6909@csjvedu.ca>
2014-04-29 20:49:04 -0400

    Added Exit Button
    Well, the button  _was_ there, but in this commit I added functionality
    to it. Now that I pretty much finished the main menu, I shall add two
    branches to the project, Singleplayer and Multiplayer, so that I can
    work on scripting the multiplayer and Raf can work on singleplayer
    without any conflicts.

---
+++ Magnum Bellum unity file/Assets/MainMenu/Scripts/ExitButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButtonScript : MonoBehaviour {
+
+	private static Color defaultColor;
+
+	void Start(){
+		defaultColor = guiTexture.color;
+	}
+
+	void OnMouseEnter(){
+		guiTexture.color = new Color(255, 0, 0, 128);
+	}
+
+	void OnMouseExit(){
+		guiTexture.color = defaultColor;
+	}
+
+	void OnMouseDown(){
+		Application.Quit();
+	}
+}
