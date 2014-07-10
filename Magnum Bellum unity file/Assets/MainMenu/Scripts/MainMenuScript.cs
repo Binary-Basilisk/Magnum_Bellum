d2159b2c82a32411abd1e1016ad1f8bdb7b1ff96
Samuel Gervais <vals6909@csjvedu.ca>
2014-07-10 15:47:55 -0400

    Improved Menu
    The menu is now way easier to manage in code and looks a bit less cheesy
    now. I shall proceed to work on the game lobby.

---
+++ Magnum Bellum unity file/Assets/MainMenu/Scripts/MainMenuScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+	private int menuId = 0;
+	public GUISkin skin;
+	public Texture background;
+	// Use this for initialization
+	void Start () {
+		enabled = true;
+		Debug.Log ("Loaded script.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI () {
+		GUI.skin = skin;
+		GUI.skin.button.fontSize = Screen.height / 16;
+		GUI.DrawTexture (new Rect(0, 0, Screen.width, Screen.height), background);
+		GUIStyle titleFontSize = new GUIStyle();
+		titleFontSize.fontSize = Screen.width / 8 + 5;
+		GUI.Label (
+			new Rect(Screen.width / 10, Screen.height / 10, Screen.width / 10 * 8, Screen.height / 10 * 8 ), 
+			"Magnum Bellum",
+			titleFontSize
+			);
+		switch(menuId){
+		case 0:
+			if (GUI.Button (new Rect(Screen.width / 4, Screen.height / 8 * 3, Screen.width / 2, Screen.height / 8), "Singleplayer")){
+			    menuId = 1;
+			}
+			if (GUI.Button (new Rect(Screen.width / 4, Screen.height / 16 * 9, Screen.width / 2, Screen.height / 8), "Multiplayer")){
+				menuId = 2;
+			}
+			if (GUI.Button (new Rect(Screen.width / 4, Screen.height / 8 * 6, Screen.width / 2, Screen.height / 8), "Exit Game")){
+				menuId = -1;
+			}
+		break;
+		case 1:
+			Debug.LogWarning("Not Implemented Yet.");
+			menuId = 0;
+		break;
+		default:
+			Debug.Log("Killing process.");
+			Application.Quit ();
+		break;
+		}
+
+	}
+}
---
+++ Magnum Bellum unity file/Assets/MainMenu/Scripts/MultiplayerButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplayerButtonScript : MonoBehaviour {
+
+	private static Color defaultColor;
+	
+	void Start(){
+		defaultColor = guiTexture.color;
+	}
+	
+	void OnMouseEnter(){
+		guiTexture.color = new Color(255, 0, 0, 128);
+	}
+	
+	void OnMouseExit(){
+		guiTexture.color = defaultColor;
+	}
+	
+	void OnMouseDown(){
+		GameObject.Find("SingleplayerButton").transform.Translate(new Vector3 (10, 0, 0));
+		GameObject.Find("MultiplayerButton").transform.Translate(new Vector3 (10, 0, 0));
+		GameObject.Find("ExitGameButton").transform.Translate(new Vector3 (10, 0, 0));
+	}
+}
